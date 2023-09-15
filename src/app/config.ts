c20a0cc54d17bf7256f534f4dbc9731976d761ed
kaiweicai <kaiweicai@163.com>
2023-09-15 23:44:14 +0800

    change dev port

--- src/app/config.ts
+++ src/app/config.ts
@@ -15,12 +15,12 @@ let prices: Price[] = [
 // const official_dev = `https://dev.voce.chat`;
 // const local_dev = `https://dev.voce.chat`;
 
-const official_dev = `http://localhost:8080`;
-const local_dev = `http://localhost:8080`;
+const official_dev = `http://localhost:3000`;
+const local_dev = `http://localhost:3000`;
 // const local_dev = `http://localhost:3000`;
 const dev_origin = process.env.REACT_APP_OFFICIAL_DEMO ? official_dev : local_dev;
 
-export const TWITTER_CLIENT_ID = 'VDJXUzBESDhhUDlyWjBDa0FtckE6MTpjaQ';
+export const TWITTER_CLIENT_ID = "VDJXUzBESDhhUDlyWjBDa0FtckE6MTpjaQ";
 
 // const local_dev = `https://im.ttt.td`;
 export const BASE_ORIGIN = process.env.REACT_APP_RELEASE ? `${location.origin}` : dev_origin;
