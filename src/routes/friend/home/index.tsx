62b58740a127085016469e1496ca881620a775d4
kaiweicai <kaiweicai@163.com>
2023-09-19 00:16:26 +0800

    add logout

--- src/routes/friend/home/index.tsx
+++ src/routes/friend/home/index.tsx
@@ -1,8 +1,10 @@
-import { memo, useEffect } from "react";
+import { memo, useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 import Manifest from "@/components/Manifest";
 import usePreload from "@/hooks/usePreload";
+import LogoutConfirmModal from "@/routes/setting/LogoutConfirmModal";
 import { NavLink, Outlet, useLocation, useMatch } from "react-router-dom";
+import { useSetState } from "rooks";
 
 function HomePage() {
   // preload basic data
@@ -12,7 +14,16 @@ function HomePage() {
   // if (!success) {
   //   return <Loading reload={true} fullscreen={true} context="home-route" />;
   // }
+  const [logoutConfirm, setLogoutConfirm] = useState(false);
   const linkClass = `flex items-center gap-2.5 px-3 py-2 font-semibold text-sm text-gray-600 rounded-lg md:hover:bg-gray-800/10`;
+  const toggleLogoutConfirm = () => {
+    setLogoutConfirm(false);
+  };
+
+  const userLogout = () => {
+    setLogoutConfirm(true);
+  };
+
   return (
     <div className="flex-center h-screen dark:bg-gray-700">
       <div className="relative py-8 px-10 shadow-md rounded-xl">
@@ -28,6 +39,14 @@ function HomePage() {
           <NavLink className="bg-primary-400 md:hover:bg-primary-400" to={"/register"}>
             sign in
           </NavLink>
+        </div>
+        <div className="flex-center flex-col pb-6">
+          <div className="danger" onClick={userLogout}>
+            logout
+          </div>
+          <div className="danger">
+            {logoutConfirm && <LogoutConfirmModal closeModal={toggleLogoutConfirm} />}
+          </div>
         </div>
       </div>
     </div>
