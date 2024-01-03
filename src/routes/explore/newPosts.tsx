04b2ecde17b8a424820bc2a005925bfabcfaff3b
cloudwei <cloudweisz@163.com>
2024-01-03 21:16:59 +0800

    add user share

--- src/routes/explore/newPosts.tsx
+++ src/routes/explore/newPosts.tsx
@@ -9,6 +9,8 @@ import { getContract } from "../wallet";
 import { useLazyGetWalletByUidQuery } from "@/app/services/user";
 import { convertToRelativeTime } from "@/utils";
 import BuyShare from "@/components/BuyShare";
+import { NavLink } from "react-router-dom";
+import UserShare from "@/components/UserShare";
 
 function newPostPage() {
   const ref = useRef<HTMLUListElement | null>(null);
@@ -45,50 +47,68 @@ function newPostPage() {
   useEffect(() => {
     getNewTwitterInfo();
   }, []);
-
-  return (
-    <ul
-      className="flex flex-col gap-1 w-full md:w-[512px] mb-44 max-h-[800px] overflow-y-scroll"
-      ref={ref}
-    >
-      <ViewportList viewportRef={ref} items={twitterUsers}>
-        {(twitterUser: TwitterUserInfo) => {
-          return (
-            <li
-              key={twitterUser.uid}
-              // onClick={buyShare.bind(null, twitterUser.uid)}
-              className="w-full flex items-center justify-between px-3 py-2 rounded-md md:hover:bg-slate-50 md:dark:hover:bg-gray-800"
-            >
-              <div className="flex gap-4 items-stretch">
-                <div>{twitterUser.uid}</div>
-                <img
-                  className="overflow-hidden rounded-full h-12 w-12"
-                  src={twitterUser.profile_image_url}
-                  alt=""
-                />
-                <div className="flex flex-col justify-center">
-                  <span className="font-bold text-md text-gray-600 dark:text-white flex items-center gap-1">
-                    {twitterUser.username}
-                  </span>
-                  <div className="flex">
-                    <span className="text-sm text-gray-600 dark:text-white flex items-center gap-1">
-                      Created: {convertToRelativeTime(twitterUser.created_time)}&nbsp;
+  if (usersLoading && twitterUsers) {
+    return <div>loading</div>;
+  } else {
+    return (
+      <ul
+        className="flex flex-col gap-1 w-full md:w-[512px] mb-44 max-h-[800px] overflow-y-scroll"
+        ref={ref}
+      >
+        <ViewportList viewportRef={ref} items={twitterUsers.map(({ uid }) => uid)}>
+          {(uid) => {
+            return (
+              <NavLink
+                key={uid}
+                className={({ isActive }) =>
+                  `rounded-md md:hover:bg-gray-500/10 ${isActive ? "bg-gray-500/10" : ""}`
+                }
+                to={`/users/${uid}`}
+              >
+                <UserShare uid={uid} enableContextMenu={true} />
+              </NavLink>
+            );
+          }}
+        </ViewportList>
+        {/* <ViewportList viewportRef={ref} items={twitterUsers}>
+          {(twitterUser: TwitterUserInfo) => {
+            return (
+              <li
+                key={twitterUser.uid}
+                // onClick={buyShare.bind(null, twitterUser.uid)}
+                className="w-full flex items-center justify-between px-3 py-2 rounded-md md:hover:bg-slate-50 md:dark:hover:bg-gray-800"
+              >
+                <div className="flex gap-4 items-stretch">
+                  <div>{twitterUser.uid}</div>
+                  <img
+                    className="overflow-hidden rounded-full h-12 w-12"
+                    src={twitterUser.profile_image_url}
+                    alt=""
+                  />
+                  <div className="flex flex-col justify-center">
+                    <span className="font-bold text-md text-gray-600 dark:text-white flex items-center gap-1">
+                      {twitterUser.username}
                     </span>
-                    <span className="text-sm text-gray-600 dark:text-white flex items-center gap-1">
-                      | Price: {twitterUser.price / 1000000000000}
-                    </span>
-                  </div>
-                  <div>
-                    <BuyShare subjectUid={twitterUser.uid}></BuyShare>
+                    <div className="flex">
+                      <span className="text-sm text-gray-600 dark:text-white flex items-center gap-1">
+                        Created: {convertToRelativeTime(twitterUser.created_time)}&nbsp;
+                      </span>
+                      <span className="text-sm text-gray-600 dark:text-white flex items-center gap-1">
+                        | Price: {twitterUser.price / 1000000000000}
+                      </span>
+                    </div>
+                    <div>
+                      <BuyShare subjectUid={twitterUser.uid}></BuyShare>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          );
-        }}
-      </ViewportList>
-    </ul>
-  );
+              </li>
+            );
+          }}
+        </ViewportList> */}
+      </ul>
+    );
+  }
 }
 
 export default newPostPage;
